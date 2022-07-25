52d6f0b5669fbd1a840943dc737437c2fc5fc4af
Nihat <n.zaliyev@gmail.com>
2022-07-25 13:00:08 -0700

    New feature file and steps added

---
+++ src/test/java/com/cydeo/library/step_definitions/NewBookFunction_StepDef.java
@@ -0,0 +1,27 @@
+package com.cydeo.library.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class NewBookFunction_StepDef {
+    @Given("librarian is on the Home Page")
+    public void librarian_is_on_the_home_page() {
+    }
+    @When("librarian clicks on Books module")
+    public void librarian_clicks_on_books_module() {
+    }
+    @When("librarian click {string} button")
+    public void librarian_click_button(String string) {
+    }
+    @When("librarian enters BookName, ISBN, Year, Author, and Description")
+    public void librarian_enters_book_name_isbn_year_author_and_description() {
+    }
+    @When("librarian clicks save changes")
+    public void librarian_clicks_save_changes() {
+    }
+    @Then("verify a new book is added")
+    public void verify_a_new_book_is_added() {
+    }
+
+}
