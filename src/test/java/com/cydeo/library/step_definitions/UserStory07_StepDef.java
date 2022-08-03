c39385d909cc0696df865e9fad89a409cf6b41c0
Cydeo-Igor <igor@cydeo.com>
2022-08-02 23:01:37 -0700

    TASK COMPLETED!

--- src/test/java/com/cydeo/library/step_definitions/UserStory07_StepDef.java
+++ src/test/java/com/cydeo/library/step_definitions/UserStory07_StepDef.java
@@ -13,12 +13,15 @@ import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class UserStory07_StepDef {
     String borrowedBookName;
     String dateBorrowed;
     LoginPage login = new LoginPage();
     BooksModule books = new BooksModule();
     BasePage base = new BasePage();
+    List<WebElement> tableRows;
 
     @Given("the student on the home page")
     public void theStudentOnTheHomePage() {
@@ -42,10 +45,11 @@ public class UserStory07_StepDef {
     public void verify_that_book_is_shown_in_page(String borrowingBooks) {
         base.borrowedBooks.click();
         BrowserUtils.waitFor(3);
-        WebElement nameOfBorrowedBook = Driver.getDriver().findElement(By.xpath("//tr[14]/td[2]"));
-
-        dateBorrowed = Driver.getDriver().findElement(By.xpath("//tr[14]/td[3]")).getText();
-        BrowserUtils.waitFor(3);
+        tableRows = Driver.getDriver().findElements(By.xpath("//tr")); // finding elements for each table row because it is dinamic, each table row is created when we borrow book
+        int tableRawData = tableRows.size()-1; // setting up my index for finding the specific data at this specifi row
+        WebElement nameOfBorrowedBook = Driver.getDriver().findElement(By.xpath("//tr["+tableRawData+"]/td[2]")); // inserting table row. size -1 to get the last row
+        dateBorrowed = Driver.getDriver().findElement(By.xpath("//tr["+tableRawData+"]/td[3]")).getText(); // finding the date of the that borrowed book since it will help us in the query to locate our book easier
+        BrowserUtils.waitFor(1);
         Assertions.assertEquals(borrowedBookName, nameOfBorrowedBook.getText());
 
     }
@@ -53,9 +57,9 @@ public class UserStory07_StepDef {
     @Then("verify logged student has same book in database")
     public void verify_logged_student_has_same_book_in_database() {
         BrowserUtils.waitFor(2);
-        DBUtilities.runQuery("select name, borrowed_date from book_borrow inner join books b on book_borrow.book_id = b.id where borrowed_date ='" +dateBorrowed+ "'");
-        System.out.println(dateBorrowed);
-        String DBbookname = DBUtilities.getFirstRowFirstColumn();
+        DBUtilities.runQuery("select name, borrowed_date from book_borrow inner join books b on book_borrow.book_id = b.id where borrowed_date ='" +dateBorrowed+ "'"); // running query to find the book where the borrowed date was found on UI
+        System.out.println(dateBorrowed); // printing the result just to confirm
+        String DBbookname = DBUtilities.getFirstRowFirstColumn(); // getting the name of the book from DB
         Assertions.assertEquals(borrowedBookName, DBbookname);
     }
 
